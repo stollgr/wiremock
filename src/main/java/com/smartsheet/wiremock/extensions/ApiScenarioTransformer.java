8af10776bc709525b3576bb9f64e1aaafc89529a
stollgr <stollgr@gmail.com>
2017-10-26 14:01:39 -0700

    Added query parameter diff check.
    Improved url match diff check to only diff on url before "?"

--- src/main/java/com/smartsheet/wiremock/extensions/ApiScenarioTransformer.java
+++ src/main/java/com/smartsheet/wiremock/extensions/ApiScenarioTransformer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.common.Errors;
 import com.github.tomakehurst.wiremock.common.FileSource;
 import com.github.tomakehurst.wiremock.common.Json;
 import com.github.tomakehurst.wiremock.common.TextFile;
@@ -12,6 +13,7 @@ import com.github.tomakehurst.wiremock.extension.Parameters;
 import com.github.tomakehurst.wiremock.extension.ResponseDefinitionTransformer;
 import com.github.tomakehurst.wiremock.http.*;
 import joptsimple.internal.Strings;
+import org.json.JSONObject;
 import org.skyscreamer.jsonassert.JSONCompare;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.skyscreamer.jsonassert.JSONCompareResult;
@@ -26,19 +28,21 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 	private static final String CONTENT_TYPE_HEADER_NAME = "Content-Type";
 	private static final String JSON_MIME_TYPE = "application/json";
 	private static final String SCENARIO_SCENARIO_FIELD = "scenario";
-	private static final String SCENARIO_REQUEST_FIELD = "request";
+    private static final String SCENARIO_QUERY_PARAMETERS_FIELD = "queryParameters";
+    private static final String SCENARIO_REQUEST_FIELD = "request";
 	private static final String SCENARIO_BODY_FIELD = "body";
 	private static final String SCENARIO_URL_PATH_FIELD = "urlPath";
 	private static final String SCENARIO_HEADERS_FIELD = "headers";
 	private static final String SCENARIOS_DIR = "__scenarios";
 	private static final String SCENARIOS_FILE = "scenarios.json";
+	private static final Integer SMARTSHEET_ERROR_CODE = 9999;
 
 	private static final ResponseDefinition INVALID_SCENARIO_RESPONSE =
 			new ResponseDefinitionBuilder()
 					.withStatus(404)
 					.withStatusMessage("Not Found")
 					.withHeader(CONTENT_TYPE_HEADER_NAME, JSON_MIME_TYPE)
-					.withBody(ErrorBody.forMessage("No scenario provided"))
+					.withBody(ErrorBody.forMessage("No scenario provided", SMARTSHEET_ERROR_CODE))
 					.build();
 
 	@Override
@@ -72,7 +76,7 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 		return new ResponseDefinitionBuilder()
 				.withStatus(400)
 				.withStatusMessage("Bad Request")
-				.withBody(ErrorBody.forMessage(diff))
+				.withBody(ErrorBody.forMessage(diff, SMARTSHEET_ERROR_CODE))
 				.build();
 	}
 
@@ -110,26 +114,11 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 	}
 
 	private static boolean scenarioHeaderIsValid(Request request) {
-		HttpHeader scenarioHeader = request.header(SCENARIO_HEADER_NAME);
+        HttpHeader scenarioHeader = request.header(SCENARIO_HEADER_NAME);
 
-		if(!scenarioHeader.isPresent()) return false;
+        return scenarioHeader.isPresent() && scenarioHeader.isSingleValued();
 
-		if(!scenarioHeader.isSingleValued()) return false;
-
-		return true;
-	}
-
-	private static ResponseDefinition buildMalformedScenarioResponse(HttpHeader scenarioHeader){
-		String scenarios = Strings.join(scenarioHeader.values(), ",");
-		String body = ErrorBody.forMessage("Invalid Scenario: " + scenarios);
-
-		return new ResponseDefinitionBuilder()
-				.withStatus(400)
-				.withStatusMessage("Bad Request")
-				.withHeader(CONTENT_TYPE_HEADER_NAME, JSON_MIME_TYPE)
-				.withBody(body)
-				.build();
-	}
+    }
 
 	private ResponseDefinition buildUnknownScenarioResponse(Request request) {
 		String scenarioName = getScenarioName (request);
@@ -137,37 +126,41 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 				.withStatus(404)
 				.withStatusMessage("Not Found")
 				.withHeader(CONTENT_TYPE_HEADER_NAME, JSON_MIME_TYPE)
-				.withBody(ErrorBody.forMessage("No scenario exists with provided name: " + scenarioName))
+				.withBody(ErrorBody.forMessage("No scenario exists with provided name: " + scenarioName, SMARTSHEET_ERROR_CODE))
 				.build();
 	}
 
 	public static final class ErrorBody {
 		private final String message;
+		private final Integer errorCode;
 
 		@JsonCreator
-		ErrorBody(@JsonProperty ("message") String message) {
+		ErrorBody(@JsonProperty ("message") String message, @JsonProperty ("errorCode") Integer errorCode) {
 			this.message = message;
+			this.errorCode = errorCode;
 		}
 
 		public String getMessage() {
 			return message;
 		}
+		public Integer getErrorCode () {return errorCode; }
 
 		@JsonIgnore
 		String toJson() {
 			return Json.write(this);
 		}
 
-		static String forMessage(String message) {
-			return new ErrorBody(message).toJson();
+		static String forMessage(String message, Integer errorCode) {
+			return new ErrorBody(message, errorCode).toJson();
 		}
 	}
 
 	private static class RequestDiff {
 		private static String getDiff(Request request, JsonNode scenario) {
-			return diffHeaders(request, scenario) +
-					diffUrl(request, scenario) +
-					diffBody(request, scenario);
+			return diffHeaders(request, scenario) + " " +
+					diffUrl(request, scenario) + " " +
+                    diffQueryParams(request, scenario) + " " +
+					diffBody(request, scenario).trim();
 		}
 
 		private static String diffBody(Request request, JsonNode scenario) {
@@ -197,20 +190,29 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 		}
 
 		private static String diffUrl(Request request, JsonNode scenario) {
-			JsonNode scenarioRequest = scenario.get(SCENARIO_REQUEST_FIELD).get(SCENARIO_URL_PATH_FIELD);
-			if(scenarioRequest == null){
+			JsonNode scenarioURL = scenario.get(SCENARIO_REQUEST_FIELD).get(SCENARIO_URL_PATH_FIELD);
+			if(scenarioURL == null){
 				return "Test scenario's request URL path was not defined or failed to parse.";
 			}
-			String scenarioRequestString = scenarioRequest.textValue();
-			if (request.getUrl().equals(scenarioRequestString)) {
+			String scenarioURLString = scenarioURL.textValue();
+
+			String requestURLString = request.getUrl().split("\\?")[0];
+
+			if (requestURLString.equals(scenarioURLString)) {
 				return "";
 			}
 
-			return formatAssert("URL", scenarioRequestString, request.getUrl());
+			return formatAssert("URL Match", scenarioURLString, requestURLString);
 		}
 
-		private static String formatAssert(String assertLabel, String expected, String actual) {
-			return String.format("%s \nExpected: %s \n     got: %s \n", assertLabel, expected, actual);
+		private static String diffQueryParams(Request request, JsonNode scenario) {
+			JsonNode scenarioQueryParams = scenario.get(SCENARIO_REQUEST_FIELD).get(SCENARIO_QUERY_PARAMETERS_FIELD);
+			if(scenarioQueryParams == null) {
+				return "";
+			}
+
+			return diffExpectedQueryParams(request, scenarioQueryParams) + diffUnexpectedQueryParams(request, scenarioQueryParams);
+
 		}
 
 		private static String diffHeaders(Request request, JsonNode scenario) {
@@ -222,15 +224,9 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 			return diffExpectedHeaders(request, scenarioHeaders);// + diffUnexpectedHeaders(request, scenarioHeaders);
 		}
 
-		private static String diffUnexpectedHeaders(Request request, JsonNode scenarioHeaders) {
-			StringBuilder unexpectedHeaderDiff = new StringBuilder();
-			for (HttpHeader header : request.getHeaders().all()) {
-				if (!scenarioHeaders.has(header.key())) {
-					unexpectedHeaderDiff.append(String.format("headers: Contains %s, but not expected\n", header.key()));
-				}
-			}
+		private static String formatAssert(String assertLabel, String expected, String actual) {
 
-			return unexpectedHeaderDiff.toString();
+			return String.format("%s Expected: %s   Got: %s", assertLabel, expected, actual);
 		}
 
 		private static String diffExpectedHeaders(Request request, JsonNode scenarioHeaders) {
@@ -248,17 +244,69 @@ public class ApiScenarioTransformer extends ResponseDefinitionTransformer {
 
 		private static String diffExpectedHeader(Request request, Map.Entry<String, JsonNode> header) {
 			if (!request.containsHeader(header.getKey())) {
-				return String.format("headers: Expected %s, but not found\n", header.getKey());
+				return String.format("Headers: Expected %s, but not found. ", header.getKey());
 			}
 
 			String headerValue = header.getValue().asText();
 			String requestValue = request.getHeader(header.getKey());
 
 			if (!headerValue.equals(requestValue)) {
-				return formatAssert("headers - " + header.getKey(), headerValue, requestValue);
+				return formatAssert("Header - " + header.getKey(), headerValue, requestValue);
 			}
 
 			return "";
 		}
+
+		private static String diffUnexpectedHeaders(Request request, JsonNode scenarioHeaders) {
+			StringBuilder unexpectedHeaderDiff = new StringBuilder();
+			for (HttpHeader header : request.getHeaders().all()) {
+				if (!scenarioHeaders.has(header.key())) {
+					unexpectedHeaderDiff.append(String.format("Headers: Contains %s, but not expected. ", header.key()));
+				}
+			}
+
+			return unexpectedHeaderDiff.toString();
+		}
+
+		private static String diffExpectedQueryParams(Request request, JsonNode scenarioQueryParams) {
+			Iterator<Map.Entry<String, JsonNode>> scenarioQueryParamIterator = scenarioQueryParams.fields();
+
+			StringBuilder queryParamDiff = new StringBuilder();
+			while(scenarioQueryParamIterator.hasNext()) {
+				Map.Entry<String, JsonNode> queryParam = scenarioQueryParamIterator.next();
+
+				queryParamDiff.append(diffExpectedQueryParam(request, queryParam));
+			}
+
+			return queryParamDiff.toString();
+		}
+
+		private static String diffExpectedQueryParam(Request request, Map.Entry<String, JsonNode> queryParam) {
+		    QueryParameter requestParam = request.queryParameter(queryParam.getKey());
+		    if(requestParam == null || requestParam.key() == null || requestParam.key().isEmpty()){
+				return String.format("Query Parameters: Expected %s, but not found. ", queryParam.getKey());
+			}
+
+			if (!requestParam.containsValue(queryParam .getValue().asText())) {
+				return formatAssert("Query Parameter - " + queryParam.getKey(), queryParam.getValue().asText(), requestParam.firstValue());
+			}
+
+			return "";
+		}
+
+        private static String diffUnexpectedQueryParams(Request request, JsonNode scenarioQueryParams) {
+            StringBuilder unexpectedQueryParamDiff = new StringBuilder();
+
+//            request.queryParameter();
+//
+//            for (HttpHeader header : request.queryParameter()) {
+//                if (!scenarioQueryParams.has(header.key())) {
+//                    unexpectedHeaderDiff.append(String.format("Headers: Contains %s, but not expected. ", header.key()));
+//                }
+//            }
+//
+//            return unexpectedHeaderDiff.toString();
+            return "";
+        }
 	}
 }
